44259e4c7d93e9e6098d2755f9820e6762f8c437
a2wd <github@a2wd.com>
2016-03-28 23:05:19 +0100

    Intial commit from local

---
+++ Todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by chayim on 28/03/2016.
+//  Copyright © 2016 UofT.a2wd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
